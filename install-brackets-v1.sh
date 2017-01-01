5cd9a312906988bcbf2e6780fbce2aaadf707561
Erik Dubois <erik.dubois@gmail.com>
2017-01-01 17:18:10 +0100

    Comment :  on 17:18:10 01-01-2017

--- install-brackets-v1.sh
+++ install-brackets-v1.sh
@@ -12,18 +12,18 @@
 ##################################################################################################################
 
 
-# dependancy
+https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
 
-wget http://ftp.de.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
-wget https://github.com/adobe/brackets/releases/download/release-1.7/Brackets.Release.1.7.64-bit.deb
-
-
-sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_amd64.deb
-sudo dpkg -i Brackets.Release.1.7.64-bit.deb
+#wget http://ftp.de.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+wget https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
 
 
-rm Brackets.Release.1.7.64-bit.deb
-rm libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+#sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+sudo dpkg -i Brackets.Release.1.8.64-bit.deb
+
+
+rm Brackets.Release.1.8.64-bit.deb
+#rm libgcrypt11_1.5.0-5+deb7u4_amd64.deb
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
