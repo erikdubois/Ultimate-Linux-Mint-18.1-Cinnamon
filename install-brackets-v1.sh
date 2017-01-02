034c6a24b7d5882527ceeec59babf672732bf2be
Erik Dubois <erik.dubois@gmail.com>
2017-01-02 12:40:22 +0100

    Comment :  on 12:40:22 02-01-2017

--- install-brackets-v1.sh
+++ install-brackets-v1.sh
@@ -15,18 +15,16 @@
 
 sudo apt install -y libgcrypt11 libcurl3
 
-https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
-
-#wget http://ftp.de.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+wget http://ftp.de.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_amd64.deb
 wget https://github.com/adobe/brackets/releases/download/release-1.8/Brackets.Release.1.8.64-bit.deb
 
 
-#sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_amd64.deb
 sudo dpkg -i Brackets.Release.1.8.64-bit.deb
 
 
 rm Brackets.Release.1.8.64-bit.deb
-#rm libgcrypt11_1.5.0-5+deb7u4_amd64.deb
+rm libgcrypt11_1.5.0-5+deb7u4_amd64.deb
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
