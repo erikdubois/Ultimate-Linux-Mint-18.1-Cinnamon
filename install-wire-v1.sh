b6e7b604f683358cfc25e6327b7c0d8e698931a4
Erik Dubois <erik.dubois@gmail.com>
2017-01-02 12:55:59 +0100

    Comment :  on 12:55:59 02-01-2017

--- install-wire-v1.sh
+++ install-wire-v1.sh
@@ -14,7 +14,7 @@
 
 rm /tmp/wire_2.11.2661_amd64.deb
 
-wget https://wire-app.wire.com/linux/wire_2.11.2661_amd64.deb -O /tmp/wire_2.11.2661_amd64.deb
+wget https://wire-app.wire.com/linux/wire_2.11.2685_amd64.deb -O /tmp/wire_2.11.2661_amd64.deb
 sudo apt install -y libjpeg62:i386
 sudo dpkg -i /tmp/wire_2.11.2661_amd64.deb
 
