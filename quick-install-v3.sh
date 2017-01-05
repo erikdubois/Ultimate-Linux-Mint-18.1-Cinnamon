7f222a3eb4a037efb0c2d7dcdbfac07fe38610f6
Erik Dubois <erik.dubois@gmail.com>
2017-01-05 09:59:26 +0100

    Comment :  on 09:59:26 05-01-2017

--- quick-install-v3.sh
+++ quick-install-v3.sh
@@ -18,41 +18,63 @@ sh 1-install-core-software-v1.sh
 sh 2-install-extra-software-v1.sh
 sh 3-install-themes-icons-cursors-conky-plank.sh
 
+### use of hashtags
+### one hashtag application is in sh 1-install-core-software-v1.sh
+### two hashtags application is in sh 2-install-extra-software-v1.sh
+### three hashtags application is in sh 3-install-themes-icons-cursors-conky-plank.sh
+### #* not installed anywhere
 
+### this is the complete list of all scripts
 
-
-sh install-atom-v1.sh
-sh install-boot-repair-v1.sh
-sh install-brackets-v1.sh
+###sh conky-aureola-v2.sh
+## hardcodefixer-v1.sh - need to run this at the very end
+###sh icons-sardi-extra-v2.sh
+###sh icons-sardi-v2.sh
+#*sh icons-sardi-vintage-v1.sh
+###sh icons-surfn-v2.sh
+###sh install-arc-flatabulous-theme-v1.sh
+##sh install-atom-v1.sh
+#*sh install-boot-repair-v1.sh
+##sh install-brackets-v1.sh
 sh install-canon-mg7750-v1.sh
-
-sh install-dconf-editor-v1.sh
-sh install-discord-canary-v1.sh
-sh install-etcher-v1.sh
-sh install-franz-v1.sh
-sh install-gradio-v2.sh
-
-sh install-mint-y-theme-github-v1.sh
-
-
-sh install-redshift-v1.sh
-
-sh install-skype-alpha-v1.sh
-sh install-slack-v1.sh
-sh install-stacer-v1.sh
-
-sh install-telegram-v1.sh
-sh install-ukuu-v1.sh
-
-
-sh install-visual-studio-code-v1.sh
-sh install-wire-v1.sh
+#*sh install-discord-canary-v1.sh
+#*sh install-etcher-v1.sh
+#*sh install-franz-v1.sh
+#*sh install-gimp-v1.sh
+#*sh install-gitkraken-v1.sh
+##sh install-google-chrome-v1.sh
+##sh install-gradio-v2.sh
+#*sh install-grub-customizer-v1.sh
+###sh install-gtk-arc-theme-v1.sh
+#*sh install-mint-y-theme-github-v1.sh
+##sh install-neofetch-v1.sh
+#*sh install-numix-v1.sh
+##sh install-peek-v2.sh
+#*sh install-pepper-flash-v1.sh
+##sh install-redshift-v1.sh
+#*sh install-remarkable-v1.sh
+##sh install-simplescreenrecorder-v1.sh
+#*sh install-skype-alpha-v1.sh
+#*sh install-slack-v1.sh
+##sh install-spotify-v2.sh
+#*sh install-stacer-v1.sh
+##sh install-sublime-text-v1.sh
+#*sh install-svgo-v1.sh
+#*sh install-telegram-v1.sh
+##sh install-teamviewer-v1.sh
+#*sh install-telegram-v1.sh
+#*sh install-tomahawk-v1.sh
+#*sh install-ukuu-v1.sh
+##sh install-variety-v1.sh
+#*sh install-virtual-box-v1.sh
+#*sh install-visual-studio-code-v1.sh
+##sh install-vivaldi-v1.sh
+#*sh install-wire-v1.sh
 sh install-zsh-v1.sh
+###sh plank-themes-v2.sh
 
-sh plank-themes-v2.sh
-
-sh hardcodefixer-v1.sh
-
+###sh theme-arc-colora-collection-3.2-v1.sh
+###sh theme-mint-y-colora-collection-3.2-v1.sh
 
 
 echo "Starting full maintenance"
@@ -68,7 +90,7 @@ echo "################################################################"
 echo "###################    T H E   E N D      ######################"
 echo "################################################################"
 
-
+sh hardcodefixer-v1.sh
 
 
 sh update-to-the-last-stable-4.9-0-v1.sh
@@ -81,8 +103,10 @@ echo "This is for the script of ZSH - Colouring of your terminal"
 echo "You do not get the chance to type your password"
 echo "Retype this line again and fill in your own username"
 echo "sudo chsh username -s /bin/zsh"
+echo "sudo chsh username -s /bin/zsh"
+echo "sudo chsh username -s /bin/zsh"
+echo "sudo chsh username -s /bin/zsh"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo
 echo
 echo "Log off or reboot to see the change in your terminal".
 echo "----------------------------------------------------"
