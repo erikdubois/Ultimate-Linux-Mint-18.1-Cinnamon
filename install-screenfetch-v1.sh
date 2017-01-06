4fa6b2b0da83f2ebc09dbceca2b8b452445e088d
Erik Dubois <erik.dubois@gmail.com>
2017-01-06 08:35:03 +0100

    Comment :  on 08:35:03 06-01-2017

---
+++ install-screenfetch-v1.sh
@@ -0,0 +1,70 @@
+#!/bin/bash 
+############################################################################
+#
+#                                       
+#                          ./+o+-       
+#                  yyyyy- -yyyyyy+      
+#               ://+//////-yyyyyyo      
+#           .++ .:/++++++/-.+sss/`      
+#         .:++o:  /++++++++/:--:/-      
+#        o:+o+:++.`..```.-/oo+++++/     
+#       .:+o:+o/.          `+sssoo+/    
+#  .++/+:+oo+o:`             /sssooo.   
+# /+++//+:`oo+o               /::--:.   
+# \+/+o+++`o++o               ++////.   
+#  .++.o+++oo+:`             /dddhhh.   
+#       .+.o+oo:.          `oddhhhh+    
+#        \+.++o+o``-````.:ohdhhhhh+     
+#         `:o+++ `ohhhhhhhhyo++os:      
+#           .o:`.syhhhhhhh/.oo++o`      
+#               /osyyyyyyo++ooo+++/     
+#                   ````` +oo+++o\:    
+#                          `oo++.      
+#                                
+#
+#
+############################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+############################################################################
+# More from Erik Dubois
+#
+# Aurora conky and all its components are installed via an other script
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/
+#
+# Sardi icons can be found at
+# http://sourceforge.net/projects/sardi/
+#
+# Yltra flat icons can be found at 
+# https://github.com/erikdubois/yltra-flat-icon-theme
+############################################################################
+#
+# the option -y has been added. It will autoinstall all. Omit if you do not want that.
+#
+############################################################################
+
+
+############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+############################################################################
+
+
+# repo for screenfetch
+# sudo add-apt-repository ppa:djcj/screenfetch -y
+# in standard repo of ubuntu
+
+# getting new info of this new repo
+sudo apt-get -y update
+
+# installing
+sudo apt-get install -y screenfetch
+
+
+############################################################################
+
+#ending
+
