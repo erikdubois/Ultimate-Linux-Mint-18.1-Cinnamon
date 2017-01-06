baabe6fc919c384ea050061a31d35eda2d88fcb2
Erik Dubois <erik.dubois@gmail.com>
2017-01-06 10:16:45 +0100

    Comment :  on 10:16:45 06-01-2017

--- install-vivaldi-v2.sh
+++ install-vivaldi-v2.sh
@@ -14,16 +14,14 @@
 
 
 
-rm /tmp/vivaldi-stable_1.4.589.41-1_amd64.deb
+rm /tmp/vivaldi-stable.deb
+
 sudo apt install -y libcurl3
 sudo apt-get -f install
-wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.4.589.41-1_amd64.deb -O /tmp/vivaldi-stable_1.4.589.41-1_amd64.deb
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.6.689.46-1_amd64.deb -O /tmp/vivaldi-stable.deb
+sudo dpkg -i /tmp/vivaldi-stable.deb
 
-sudo dpkg -i /tmp/vivaldi-stable_1.4.589.41-1_amd64.deb
-
-
-
-rm /tmp/vivaldi-stable_1.4.589.41-1_amd64.deb
+rm /tmp/vivaldi-stable.deb
 
 
 ##################################################################################################################
