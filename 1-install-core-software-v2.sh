51f59f3d43cf3f0c3a9b95465217e9baf72eb6cc
Erik Dubois <erik.dubois@gmail.com>
2017-01-08 12:54:31 +0100

    Comment :  on 12:54:31 08-01-2017

--- 1-install-core-software-v2.sh
+++ 1-install-core-software-v2.sh
@@ -15,7 +15,7 @@
 sudo apt-get install -y adobe-flashplugin catfish clementine curl dconf-cli dconf-editor dropbox evolution focuswriter geary gimp gpick
 sudo apt-get install -y glances gparted grsync hardinfo inkscape meld nemo-dropbox
 sudo apt-get install -y openshot pinta plank ppa-purge radiotray screenruler screenfetch scrot shutter slurm synapse
-sudo apt-get install -y terminator thunar vlc vnstat   
+sudo apt-get install -y thunar vlc vnstat   
 
 
 #operating specific software
