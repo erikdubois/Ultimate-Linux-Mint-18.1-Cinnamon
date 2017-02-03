358c85f92dcefa73b1ee6f6ec6ac2a29b6fc1bcd
Erik Dubois <erik.dubois@gmail.com>
2017-02-03 22:51:17 +0100

    Comment :  on Fri Feb  3 22:51:17 CET 2017

--- setup-git.sh
+++ setup-git.sh
@@ -45,6 +45,29 @@ if ! location="$(type -p "git")" || [ -z "git" ]; then
 	echo "#################################################"
 
   	sudo apt install git -y
+	# check if apt-git is installed
+	if which apt-get > /dev/null; then
+
+		sudo apt-get install -y git
+
+	fi
+
+	# check if pacman is installed
+	if which pacman > /dev/null; then
+
+		sudo pacman -S --noconfirm git
+
+	fi
+
+	# check if eopkg is installed
+	if which eopkg > /dev/null; then
+
+		sudo eopkg -y install git
+
+	fi
+
+
+
 fi
 
 
