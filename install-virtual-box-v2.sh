abc69f6fbfd3f3e43a929fb4f15ebdf1a740ca40
Erik Dubois <erik.dubois@gmail.com>
2017-03-17 12:50:23 +0100

    Comment :  on Fri Mar 17 12:50:22 CET 2017

--- install-virtual-box-v2.sh
+++ install-virtual-box-v2.sh
@@ -15,8 +15,9 @@
 rm /tmp/virtual-box.deb
 
 #wget http://download.virtualbox.org/virtualbox/5.1.12/virtualbox-5.1_5.1.12-112440~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
-wget http://download.virtualbox.org/virtualbox/5.1.14/virtualbox-5.1_5.1.14-112924~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
-#curl -o /tmp/code_1.5.3-1474533365_amd64.deb https://code.visualstudio.com/docs/?dv=linux64_deb
+#wget http://download.virtualbox.org/virtualbox/5.1.14/virtualbox-5.1_5.1.14-112924~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
+wget http://download.virtualbox.org/virtualbox/5.1.18/virtualbox-5.1_5.1.18-114002~Ubuntu~xenial_amd64.deb -O /tmp/virtual-box.deb
+
 sudo dpkg -i /tmp/virtual-box.deb
 #gdebi /tmp/code_1.5.3-1474533365_amd64.deb
 
